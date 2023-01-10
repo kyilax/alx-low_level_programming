f7f3e191526dd29f34d25716a7df570ecca137d9
Osariemen Daniel Osazee <63324071+kyilax@users.noreply.github.com>
2023-01-11 00:18:52 +0100

    new leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,23 +1,21 @@
-
 char *leet(char *s)
 {
-    int letter[] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
-    int num_code[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
+        int letter[] = {97, 65, 101, 69, 111, 79, 116, 84, 108, 76};
+        int num_code[] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
+        int count = 0, i;
 
-    int count = 0, i;
-
-    while (*(s + count) != '\0')
-    {
-        for (i = 0; i < 10; i++)
+        while (*(s + count) != '\0')
         {
-            if (*(s + count) == letter[i])
-            {
-                *(s + count) = num_code[i];
-            }
+                for (i = 0; i < 10; i++)
+                {
+                        if (*(s + count) == letter[i])
+                        {
+                                *(s + count) = num_code[i];
+                        }
+                }
+
+                count++;
         }
 
-        count++;
-    }
-
-    return s;
+        return (s);
 }
